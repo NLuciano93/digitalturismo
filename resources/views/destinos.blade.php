d8f32a210f527b39d849b5206ddb51d6dcd1c196
Pablo Sergio <pablopetito@gmail.com>
2020-02-18 18:52:36 -0300

    pagina destino - dando formato

--- resources/views/destinos.blade.php
+++ resources/views/destinos.blade.php
@@ -1,28 +1,22 @@
-@extends('layout.plantillaAdmin')
-
-@section('principal')
-
-<?php 
-        // require_once 'config.php';
-        $destinos = Destino::listarDestinos();
-        $paises = Pais::listarPais();
-        $provincias = Provincia::listarProvincia();
-        if(isset($_POST["destino"])){   
-               $destinos = Destino::buscarDestinos($_POST["destino"], $_POST["provincia"]);
-        } 
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Digital Turismo | Productos</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/styleProductos.css')}}">
+    <link rel="stylesheet" href="css/style-login-registro.css">
+    <link href="https://fonts.googleapis.com/css?family=Anton|Oswald:500|Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/67f61afa3e.js" crossorigin="anonymous"></script>
+</head>
+<body>
 
 <div class="container-fluid">
 <div class="row">
 <div class="col-12 p-0 m-0">
-
-    <!-- Cabecera - barra navegadora -->
-    <div class="container-fluid contenedor-nav">
-        <div class="row p-0 m-0">
-          {{-- <?php  include_once('nav.php') ?> --}}
-        </div>
-    </div>
-    
+  
     <!-- titulo pagina -->
     <div class="fondo_productos">
         
@@ -46,7 +40,7 @@
                             <!-- imagenes carousel -->
                             <div class="carousel-inner">
                                 <div class="carousel-item active imgCarrusel">
-                                    <img class = "imgCarruseltamano" src="images/Destinos/Bariloche/barilocheBaseNieve.jpg" class="d-block w-100 img-fluid" alt="Bariloche"> 
+                                    <img class = "imgCarruseltamano" src="{{asset('images/Destinos/barilocheEsquiando.jpg')}}" class="d-block w-100 img-fluid" alt="Bariloche"> 
                                     <div class="tit-Destino-carru" >
                                         <a href="detalleProducto.php">
                                             <h3>BARILOCHE SKI</h3>
@@ -98,121 +92,36 @@
             </div>
         </div>
         
+        
         <!-- Todos los destinos  -->
-        <section class="destinos">
-        <nav class="navbar navbar-light bg-dark d-flex justify-content-center">
-            <form class="form" action="" method="post">
-             <div class="form-row">
-                <div class="form-group">
-                    <input class="form-control mr-sm-3" type="search" name='destino' value="<?= isset($_POST['destino']) ? $_POST['destino'] : '' ?>" placeholder="Buscar Destino" aria-label="Default" 
-                                    aria-describedby="inputGroup-sizing-default">
-                                     
-                </div>   
-                 
-                <div class="form-group mx-3">
-                <select id="inputState" class="form-control mr-sm-5" name="provincia">
-                    <option value="">Elegir Provincia...</option>
-                    <?php foreach($provincias as $provincia) : ?>
-                        
-                            <option value="<?= $provincia->getId()?>"><?= $provincia->getNombre() ?></option>
-                    <?php endforeach;?>
-                </select>
-                </div> 
-
-                <button class="btn btn-outline-success" type="submit">Buscar</button>
-            </div>
-            
-               
-            </form>
-        </nav>
+       
             <div class="container">
                 <div class="row">
-{{--                     <?php if (is_array($destinos)):
-                        foreach ($destinos as $destino) : ?> --}}
-                    @foreach ($destinos as $destino)        
-                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
+
+                    @foreach ($destinos as $destino)
+                        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
                                     
-                                    <article class="borderBox m-3 destino-individual">
-                                        
-                                        <div class="photo-container">
-                                            <img class="photo" src="images/Destinos/
-                                            {{$destino->avatar_destino}}" alt="Sin Imagen">
-                                            @if ({{$destino->promocion]}}) : 
-                                                <img class="special" src="images/iconos/OfertaEspecial.png" alt="promo">
-                                            @endif
-                                            <button class="favorito"><i class="fas fa-heart"></i></button>
-                                            <a href="detalleProducto.php" title="Mas Informacion">
-                                                <div class="tit-Destino" >
-                                                    <h3 class="texto-card-titulo">
-                                                    {{$destino->nombre_destino}}
-                                                    </h3>
-                                                    <h3 class="texto-card-titulo">$
-                                                    {{$destino->precio}}
-                                                    </h3>
-                                                </div>
-                                            </a>
+                            <article class="borderBox m-3 destino-individual">
+                                
+                                <div class="photo-container">
+                                    <img class="photo" src="{{asset('images/Destinos/barilocheEsquiando.jpg')}}" alt="Bari Esquiando">
+                                    <button class="favorito"><i class="fas fa-heart"></i></button>
+                                    <a href="detalleProducto.php" title="Mas Informacion">
+                                        <div class="tit-Destino" >
+                                            <h3 class="texto-card-titulo">{{$destino->nombre_destino}}</h3>
+                                            <h3 class="texto-card-titulo">${{$destino->precio}}</h3>
                                         </div>
-                                        
-                                    </article>                                
+                                    </a>
                                 </div>
-
-                    @endforeach;   
-
-                        
-                   <?php else:?>
-                    <div class="alert alert-danger" role="alert">
-                        <span class="alert-link"><?= $destinos ?></span>
-                    </div>
-                   <?php endif;?>
-                </div>
-                
-                <!-- controles de paginacion -->
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <nav aria-label="...">
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item disabled">
-                                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item active" aria-current="page">
-                                        <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                                    </li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">Next</a>
-                                    </li>
-                                </ul>
-                            </nav>
+                                
+                            </article>                                
                         </div>
-                    </div>
-                    
+                    @endforeach
                 </div>
-            </section>
-            
-        </div>
-        
-        <!--------FOOTER------------------------------->
-{{--         <div class="container-fluid">
-            <div class="row">             
-                <?php include_once('footer.php') ?>
-            </div>
-        </div> --}}
-
+            </div> 
+    </div>
 </div>
 </div>
-</div>
-
-    
-{{--         @foreach ($destinos as $destino)
-       
-            <tr>
-                <td>{{$destino->nombre_destino}}</td>
-                <td>${{$destino->precio}}</td>
-                <td>{{$destino->promocion}}%</td>
-                <td>{{$destino->provincia->nombre_provincia}}</td>
-                
-                <td><a href="destinoMod/{{$destino->id_destino}}" class="btn btn-success" tabindex="-1" role="button">Editar</a></td>
-                <td><a href="/borrarDestino/{{$destino->id_destino}}" class="btn btn-danger" tabindex="-1" role="button">Eliminar</a></td>
-            </tr>
-        @endforeach --}}
-@endsection
+</div> 
+</body>
+</html>
